7386ffbf32313fafab508f56e6bc03030d8b5ef7
DeniArif <deniarifwibowo82@gmail.com>
2024-11-21 23:10:18 +0700

    Membuat DynamicSelectTextField

--- app/src/main/java/com/example/praktikum6/ui/theme/view/widget/DynamicSelectedField.kt
+++ app/src/main/java/com/example/praktikum6/ui/theme/view/widget/DynamicSelectedField.kt
@@ -1,2 +1,16 @@
 package com.example.praktikum6.ui.theme.view.widget
 
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.lifecycle.Lifecycle
+
+@Composable
+fun DynamicSelectTextField(
+    selectedValue: String,
+    option: List<String>,
+    label: String,
+    onValueChangedEvent: (String) -> Unit,
+    modifier: Modifier = Modifier
+){
+
+}
