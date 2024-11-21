5fc55be52d08aa170b43937581433e20387e8e92
DeniArif <deniarifwibowo82@gmail.com>
2024-11-21 21:08:18 +0700

    membuat variabel mahasiswaUiState

--- app/src/main/java/com/example/praktikum6/navigasi/PengelolaHalaman.kt
+++ app/src/main/java/com/example/praktikum6/navigasi/PengelolaHalaman.kt
@@ -1,6 +1,7 @@
 package com.example.praktikum6.navigasi
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import com.example.praktikum6.model.Mahasiswa
@@ -18,4 +19,7 @@ fun MahasiswaApp(
     modifier = Modifier,
     mahasiswaViewModel: MahasiswaViewModel = viewModel(),
     navHostController: NavHostController = rememberNavController
-)
+) {
+    val mahassiswaUiState = mahasiswaViewModel.mahasiswaUiState.collectAsState().value
+
+}
