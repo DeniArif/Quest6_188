b05ebd3e34f6c5e40fbaec7ae4d71a173373f4d5
DeniArif <deniarifwibowo82@gmail.com>
2024-11-22 15:51:22 +0700

    Membuat RencanaStudyViewModel.kt

--- app/src/main/java/com/example/praktikum6/ui/theme/view/viewmodel/RencanaStudyViewModel.kt
+++ app/src/main/java/com/example/praktikum6/ui/theme/view/viewmodel/RencanaStudyViewModel.kt
@@ -1,4 +1,13 @@
 package com.example.praktikum6.ui.theme.view.viewmodel
 
+import com.example.praktikum6.model.RencanaStudi
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+
 class RencanaStudyViewModel {
+    private val _krsState = MutableStateFlow(RencanaStudi())
+    val krsStudiUi: StateFlow<RencanaStudi> = _krsState.asStateFlow()
+
+
 }
