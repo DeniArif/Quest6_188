cc4dd7c22ff2f4a34491f61ddd14ac268e2416de
DeniArif <deniarifwibowo82@gmail.com>
2024-11-22 16:51:36 +0700

    Memperbaiki eror dan membuat row dan image

--- app/src/main/java/com/example/praktikum6/ui/theme/view/screen/TampilData.kt
+++ app/src/main/java/com/example/praktikum6/ui/theme/view/screen/TampilData.kt
@@ -1,6 +1,22 @@
 package com.example.praktikum6.ui.theme.view.screen
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import com.example.praktikum6.R
 import com.example.praktikum6.model.Mahasiswa
 import com.example.praktikum6.model.RencanaStudi
 
@@ -9,7 +25,27 @@ fun TampilData(
     mahasiswa: Mahasiswa,
     rencanaStudi: RencanaStudi,
     onBackButtonClicked: () -> Unit
-){
+) {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(color = colorResource(id = R.color.primary))
+    ){
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp),
+            verticalAlignment = Alignment.CenterVertically
+        ){
+            Image(
+                painter = painterResource(id = R.drawable.umy),
+                contentDescription = "",
+                modifier = Modifier
+                    .clip(shape = CircleShape)
+                    .size(60.dp)
+            )
+        }
 
+    }
 }
 
